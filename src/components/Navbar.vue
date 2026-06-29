e510a1a9d7a4028bb174859acd284d4f79ed8e8a
Immanuel Elvando Kenjam <elvando@192.168.1.3>
2026-06-29 12:27:00 +0700

    fix search button mobile screen size

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -170,7 +170,9 @@ onUnmounted(() => {
 
         <div class="flex-1 flex justify-end items-center space-x-4">
   
-          <button class="text-gray-500 hover:text-blue-600 transition">
+          <button
+            class="text-gray-500 hover:text-blue-600 transition"
+          >
             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" />
             </svg>
@@ -181,7 +183,7 @@ onUnmounted(() => {
           <button 
             v-if="!isLoggedIn" 
             @click="router.push('/login')" 
-            class="text-gray-500 hover:text-blue-600 font-medium border border-gray-300 px-5 py-2 rounded-lg transition"
+            class="text-gray-500 sm:block hidden hover:text-blue-600 font-medium border border-gray-300 px-5 py-2 rounded-lg transition"
           >
             Login
           </button>
@@ -223,7 +225,7 @@ onUnmounted(() => {
 
         <!-- Mobile menu button -->
         <div class="flex items-center sm:hidden space-x-2">
-          <button class="text-gray-500 hover:text-pres-blue p-2 transition-colors">
+          <button @click="isSearchOpen = true" class="text-gray-500 hover:text-pres-blue p-2 transition-colors">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
